fcaef40e2b91eb9c327f38b3c1c5a623962603cd
Shubham Kadam <113159860+Shubhamkadam833@users.noreply.github.com>
2023-03-05 15:51:36 -0500

    Add files via upload

---
+++ BMI Calculator.py
@@ -0,0 +1,14 @@
+#BMI Calculator
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+Height = float(height)
+Weight = int(weight)
+    
+BMI = (Weight / (Height ** 2) )
+print(int(BMI))
+
